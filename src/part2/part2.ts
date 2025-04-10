4e7873b2c47c97b98af76e7e60b796d6d4dc92f1
Ron <ron.sh.10.01@gmail.com>
2025-04-10 21:32:58 +0300

    2.1

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -4,11 +4,18 @@ const stringToArray = R.split("");
 
 /* Question 1 */
 const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
-export const countVowels: undefined = undefined;
+export const countVowels: (s: string) => number =
+    (s: string) => (R.reduce(
+        ((acc: number, c: string) => (vowels.includes(c) ? acc + 1 : acc)),
+        0,
+        stringToArray(s)
+    )
+    );
 
+console.log(countVowels("Hellooo"));
 /* Question 2 */
 export const isPalindrome: undefined = undefined;
-  
+
 /* Question 3 */
 export type WordTree = {
     root: string;
